6b955ea9a7b6923e478a82cebb25f2c1d11c543f
Сергей Хрипко <khripko.ser@yandex.ru>
2023-07-25 20:37:35 +0300

    Задача 1_11 Координаты_обновление

--- src/main/java/ru/condition/Point.java
+++ src/main/java/ru/condition/Point.java
@@ -2,13 +2,8 @@ package ru.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double calc = x1 - x2;
-        double calc1 = y1 - y2;
-        double first = Math.pow(calc, 2);
-        double first1 = Math.pow(calc1, 2);
-        first = first + first1;
-        double fin = Math.sqrt(first);
-        return fin;
+        double rsl = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        return rsl;
     }
 
     public static void main(String[] args) {
