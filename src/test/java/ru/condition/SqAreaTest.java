e8911243aa37052da825d3678b782282afc9bf68
Сергей Хрипко <khripko.ser@yandex.ru>
2023-08-07 19:34:11 +0300

    2.6 Tест для задачи стороны прямоугольника

--- src/test/java/ru/condition/SqAreaTest.java
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -1,42 +1,35 @@
 package ru.condition;
 
-import  static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class SqAreaTest {
 
     @Test
-    void whenP6K2Square2 () {
-
+    void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
         double k = 2;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-
     }
-@Test
-    void whenP10K4Square4 () {
 
+    @Test
+    void whenP10K4Square4() {
         int expected = 4;
         int p = 10;
         double k = 4;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-
     }
 
     @Test
-    void whenP2K1Square0DOT25 () {
-
+    void whenP2K1Square0DOT25() {
         double expected = 0.25;
         int p = 2;
         double k = 1;
         double out = SqArea.square(p, k);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
-
     }
-
-
 }
