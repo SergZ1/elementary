b7d877263134bbe4fdc06566b393441db37f866f
Сергей Хрипко <khripko.ser@yandex.ru>
2023-08-28 13:58:14 +0300

    4.8 Простое число, оператор break

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,17 +2,14 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
+        boolean prime = number > 1;
         for (int count = 2; number >= count; count++) {
             if ((number % count) != 0) {
                 prime = true;
                 break;
-
-            } else if ((number % count) == 0) {
-                prime = false;
-                break;
-
             }
+        prime = number % count != 0;
+            break;
         }
         return prime;
     }
