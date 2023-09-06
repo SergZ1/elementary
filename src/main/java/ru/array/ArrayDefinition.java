7e9df4cf1b44b0ae96cb3a8f2d7d627478572bc4
Сергей Хрипко <khripko.ser@yandex.ru>
2023-09-06 19:16:50 +0300

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива ages равен " + ages.length);
-        System.out.println("Размер массива surnames равен " + surnames.length);
-        System.out.println("Размер массива prices равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Sergey Sergeev";
+        names[3] = "Nikita Kuznecov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
