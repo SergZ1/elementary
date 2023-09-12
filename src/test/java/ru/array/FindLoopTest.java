13a7bd0998c1e9c7fbea11ed75123cdd8d29a54d
Сергей Хрипко <khripko.ser@yandex.ru>
2023-09-12 19:54:19 +0300

    5.8 Классический поиск

--- src/test/java/ru/array/FindLoopTest.java
+++ src/test/java/ru/array/FindLoopTest.java
@@ -2,6 +2,7 @@
 package ru.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FindLoopTest {
@@ -13,12 +14,22 @@ class FindLoopTest {
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
+
     @Test
     void whenArrayHasNot10ThenMinus1() {
         int[] data = new int[]{1, 4, 5};
         int el = 10;
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas6ThenMinus0() {
+        int[] data = new int[]{10, 5, 6};
+        int el = 6;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
