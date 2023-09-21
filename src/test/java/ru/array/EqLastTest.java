0a85e589be2ec8fa7626eb307cfbc96a0905ee77
Сергей Хрипко <khripko.ser@yandex.ru>
2023-09-21 10:46:52 +0300

    5.15 Сравнить последние элементы двух массивов

--- src/test/java/ru/array/EqLastTest.java
+++ src/test/java/ru/array/EqLastTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class EgLastTest {
+public class EqLastTest {
     @Test
     public void whenEq() {
         int[] left = {1, 2, 3};
         int[] right = {5, 4, 3};
-        boolean result = EgLast.check(left, right);
+        boolean result = EqLast.check(left, right);
         assertThat(result).isTrue();
     }
 
@@ -17,7 +17,7 @@ public class EgLastTest {
     public void whenNotEq() {
         int[] left = {1, 2, 3};
         int[] right = {3, 3, 4};
-        boolean result = EgLast.check(left, right);
+        boolean result = EqLast.check(left, right);
         assertThat(result).isFalse();
     }
 }
