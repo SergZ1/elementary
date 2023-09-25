d44c71b4243b22afe25d62251ae93faf1480a2d1
Сергей Хрипко <khripko.ser@yandex.ru>
2023-09-25 09:22:49 +0300

    5.22 Двумерный массив

--- src/main/java/ru/array/Sequence.java
+++ src/main/java/ru/array/Sequence.java
@@ -3,18 +3,14 @@ package ru.array;
 public class Sequence {
     public static void main(String[] args) {
         int[][] array = new int[3][3];
-        array[0][1] = 1;
-        array[0][2] = 2;
-        array[0][3] = 3;
-        array[1][1] = 5;
-        array[1][2] = 6;
-        array[1][3] = 7;
-        array[2][1] = 8;
-        array[2][2] = 9;
-        array[2][3] = 10;
-        array[3][1] = 11;
-        array[3][2] = 12;
-        array[3][3] = 13;
-
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 20;
+        array[1][0] = 5;
+        array[1][1] = 6;
+        array[1][2] = 7;
+        array[2][0] = 8;
+        array[2][1] = 9;
+        array[2][2] = 10;
     }
 }
