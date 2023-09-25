1314909c3e7aed8e862a759b9c5a00c4febc4a22
Сергей Хрипко <khripko.ser@yandex.ru>
2023-09-25 09:54:25 +0300

    5.22 Двумерный массив

---
+++ src/main/java/ru/array/LengthArrayArrays.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class LengthArrayArrays {
+    public static void main(String[] args) {
+        int[][] numbers = {{2}, {10, 11}, {12, 13, 15}, {20, 30, 40, 50}};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(
+                    "Размер вложенного массива равен: " + numbers[i].length
+            );
+        }
+    }
+}
