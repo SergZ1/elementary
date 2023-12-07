2b63a9d35ceb9a6f0eeaaff71b6300b9467e0f16
Hripko <khripko.ser@yandex.ru>
2023-12-07 19:19:59 +0300

    7_16 Расстояние между точками в пространстве

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -8,8 +8,8 @@ class PointTest {
 
     @Test
     void when00To20Then2() {
-        Point a = new Point(0,0);
-        Point b = new Point(2,0);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double result = a.distance(b);
         double expected = 2.0;
         assertThat(result).isEqualTo(expected, withPrecision(0.01));
@@ -17,8 +17,8 @@ class PointTest {
 
     @Test
     void when02To02Then0() {
-        Point a = new Point(0,2);
-        Point b = new Point(0,2);
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 2);
         double result = a.distance(b);
         double expected = 0.0;
         assertThat(result).isEqualTo(expected, withPrecision(0.01));
@@ -26,8 +26,8 @@ class PointTest {
 
     @Test
     void when00To22Then2Dot8() {
-        Point a = new Point(0,0);
-        Point b = new Point(2,2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 2);
         double result = a.distance(b);
         double expected = 2.8284271247461903;
         assertThat(result).isEqualTo(expected, withPrecision(0.01));
@@ -35,8 +35,8 @@ class PointTest {
 
     @Test
     void when22To22Then0() {
-        Point a = new Point(2,2);
-        Point b = new Point(2,2);
+        Point a = new Point(2, 2);
+        Point b = new Point(2, 2);
         double result = a.distance(b);
         double expected = 0;
         assertThat(result).isEqualTo(expected, withPrecision(0.01));
@@ -44,9 +44,19 @@ class PointTest {
 
     @Test
     void when02To00Then2() {
-        Point a = new Point(0,2);
-        Point b = new Point(0,0);
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 0);
         double result = a.distance(b);
+        double expected = 2;
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when00To22To22Then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 2);
+        Point c = new Point(2, 2, 2);
+        double result = c.distance3d(b);
         double expected = 2;
         assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
